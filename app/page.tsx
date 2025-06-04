1530b76f440e21bed3e1e79ea3232ff5fd19f05d
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-06-05 04:19:22 +0530

    fix: display fix, motion added

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
-'use client';
 import Hero from '@/components/Hero';
-import { motion } from 'motion/react';
 
 export default function Home() {
   return (
-    <motion.div>
-      <Hero />
-    </motion.div>
+   <Hero />
   );
 }
