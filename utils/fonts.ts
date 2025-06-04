4eb78cd6c0702638f5f369f7edf42744762d5110
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-06-05 00:09:40 +0530

    feat: all fonts added

--- utils/fonts.ts
+++ utils/fonts.ts
@@ -1,12 +1,26 @@
-import { Bricolage_Grotesque, Montserrat, Host_Grotesk, DM_Sans } from "next/font/google";
+import { Bricolage_Grotesque, Montserrat, Host_Grotesk, DM_Sans, Bangers, Press_Start_2P, Eater, Irish_Grover, Nosifer, Kalnia_Glaze, Rubik_Gemstones } from "next/font/google";
 
 const Bricolage_font = Bricolage_Grotesque()
 const Montserrat_font = Montserrat()
 const Host_Grotesk_font = Host_Grotesk()
 const DM_Sans_font = DM_Sans()
+const Kalnia_Glaze_font = Kalnia_Glaze()
+const Rubik_Gemstones_font = Rubik_Gemstones({weight: '400'})
+const Press_Start_2P_font = Press_Start_2P({weight: '400'})
+const Bangers_font = Bangers({weight: '400'})
+const Eater_font = Eater({weight: '400'})
+const Irish_Grover_font = Irish_Grover({weight: '400'})
+const Nosifer_font = Nosifer({weight: '400'})
 
 
 export const Bricolage = Bricolage_font.className
 export const Montserratfont = Montserrat_font.className
 export const HostGrotesk = Host_Grotesk_font.className
 export const DMSans = DM_Sans_font.className
+export const KalniaGlaze = Kalnia_Glaze_font.className
+export const RubikGemstones = Rubik_Gemstones_font.className
+export const PressStart2P = Press_Start_2P_font.className
+export const Bangersfont = Bangers_font.className
+export const Eaterfont = Eater_font.className
+export const IrishGrover = Irish_Grover_font.className
+export const Nosiferfont = Nosifer_font.className
