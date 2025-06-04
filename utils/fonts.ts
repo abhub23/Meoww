d4ea8def52d4b4400ea36d96a03dd49e92acdb08
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-06-04 22:29:52 +0530

    feat: utils added

---
+++ utils/fonts.ts
@@ -0,0 +1,12 @@
+import { Bricolage_Grotesque, Montserrat, Host_Grotesk, DM_Sans } from "next/font/google";
+
+const Bricolage_font = Bricolage_Grotesque()
+const Montserrat_font = Montserrat()
+const Host_Grotesk_font = Host_Grotesk()
+const DM_Sans_font = DM_Sans()
+
+
+export const Bricolage = Bricolage_font.className
+export const Montserratfont = Montserrat_font.className
+export const HostGrotesk = Host_Grotesk_font.className
+export const DMSans = DM_Sans_font.className
