a9d0304414b51c5210ba09f776c500cabbc6168f
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-07-18 00:11:25 +0530

    some changes in footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,20 +2,14 @@ import Link from 'next/link';
 
 const Footer = () => {
   return (
-    <div className="fixed bottom-3 left-1/2 w-full -translate-x-1/2 transform text-center text-[12px] lg:text-[16px] dark:text-white">
+    <div className="fixed bottom-3 left-1/2 w-full -translate-x-1/2 transform text-center text-[12px] text-black/60 lg:text-[16px] dark:text-white/60">
       Designed and Developed by
       <Link
-        className="ml-1 text-transparent"
-        style={{
-          background:
-            'linear-gradient(to right, oklch(71.8% 0.202 349.761), oklch(74% 0.238 322.16), oklch(70.2% 0.183 293.541))',
-          WebkitBackgroundClip: 'text',
-          backgroundClip: 'text',
-        }}
+        className="ml-1 font-medium text-black dark:text-white"
         href="https://x.com/abdullah_twt23"
         target="_black"
       >
-        Abdullah Mukri
+        Abdullah
       </Link>
     </div>
   );
