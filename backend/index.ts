8337e43d0d282d383e7e8ea2933655126bd8693d
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-07-17 23:57:33 +0530

    added cron job for random image URLs

--- backend/index.ts
+++ backend/index.ts
@@ -1,19 +1,54 @@
-import express from 'express'
-import cors from 'cors'
+import dotenv from 'dotenv';
+import express from 'express';
+import cron from 'node-cron';
+import cors from 'cors';
+import axios from 'axios';
 
+dotenv.config({ path: '../.env' });
 
-const app = express()
-app.use(express.json())
+const api_secret = process.env.CATAPI;
+
+const app = express();
+app.use(express.json());
 app.use(cors({
-    origin: '*'
-}))
+  origin: '*',
+})
+);
 
 const PORT = 8090;
 
-app.get('/' , (_, res) => {
-    res.json({message: 'Server is alive'})
-})
+app.get('/', (_, res) => {
+  res.json({ message: 'Server is alive' });
+});
 
-app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`)
-})
+let caturl: string;
+
+cron.schedule('* * * * *', async () => {
+  try {
+    const response = await axios.get('https://api.thecatapi.com/v1/images/search', {
+      headers: {
+        'x-api-key': api_secret,
+      },
+    });
+
+    caturl = response.data[0].url;
+    console.log(caturl);
+  } catch (error) {
+    console.error('Error occured in Api call: ', error);
+  }
+});
+
+app.get('/getimage', async (_, res) => {
+  res.status(200).json({
+    caturl,
+    success: true,
+  });
+});
+
+if (process.env.VERCEL !== '1') {
+  app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Local Server listening on port ${PORT}`);
+  });
+}
+
+export default app;
