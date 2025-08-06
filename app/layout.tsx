0257699e55470674c1ddd6a3d8da4c77d9f36054
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-08-06 22:48:46 +0530

    added twitter card to the site and metadata for seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,12 +5,58 @@ import { QueryProvider } from '@/lib/QueryProvider';
 import './globals.css';
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://meoww.abdullahtech.dev'),
   title: 'Meoww - Talk with your loved ones',
   icons: {
     icon: '/cat.webp',
   },
   description: 'Application to talk with cats with some minimum set of convos',
+
+  keywords: [
+    'Cats',
+    'Cat voices',
+    'Catsvoice',
+    'cat voices',
+    'Meoww App',
+    'Meoww Website',
+    'Cat website',
+    'Animal Voices',
+    'Pss-Pss-Pss',
+    'All about cat',
+    'cute cats voices',
+    'voices of animals',
+    'cat and talk',
+    'cats are adorable',
+    'cats for the president',
+    'cats are the best',
+  ],
+  authors: {
+    url: 'https://github.com/abhub23',
+    name: 'Abdullah Mukri',
+  },
+  publisher: 'Abdullah Mukri',
+
+  twitter: {
+    card: 'summary_large_image',
+    site: '@abdullah_twt23',
+    creator: '@abdullah_twt23',
+    title: 'abdullah_twt23',
+    description: 'Talk with your Cat',
+    images: {
+      url: 'https://abdullahtech.dev/og-image.png',
+      type: 'image/png',
+    },
+  },
+
+  appLinks: {
+    web: {
+      url: new URL('https://meoww.abdullahtech.dev'),
+    },
+  },
+
+  category: 'Animal Audios',
 };
+
 
 export default function RootLayout({
   children,
